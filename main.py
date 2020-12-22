2de9e35cce2574074c4d4e37b4cfc23cf2d947d3
sam505 <mainasamuel505@gmail.com>
2020-12-22 17:39:17 +0300

    Making it better

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import pandas as pd
-import quandl
+import datetime
 import math
+import pickle
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import preprocessing, svm
-from sklearn.model_selection import train_test_split
+import quandl
+from matplotlib import style
+from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -17,24 +22,41 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_Change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)  # Replace missing data points
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(["label"], 1))
-
-y = np.array(df['label'])
-
 X = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+df.dropna(inplace=True)
 
 y = np.array(df['label'])
-
-print(len(X), len(y))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 classifier = LinearRegression(n_jobs=10)
 classifier.fit(X_train, y_train)
 accuracy = classifier.score(X_test, y_test)
-print(accuracy)
+forecast_set = classifier.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_data = df.iloc[-1].name
+last_unix = last_data.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=2)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
