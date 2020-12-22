6445c88ea9511226c99e3fc97e92af7b39bc13b9
sam505 <mainasamuel505@gmail.com>
2020-12-22 20:20:44 +0300

    Making it better

---
+++ raw_linear_regression.py
@@ -0,0 +1,14 @@
+from statistics import mean
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+y = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+
+def slope_line_best_fit(x, y):
+
+    return gradient
+
+
+m = slope_line_best_fit(x, y)
